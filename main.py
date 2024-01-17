d36616c26b21b95e9cd44c12b1c396ca5c186e72
kekis0 <62936109+kekis0@users.noreply.github.com>
2024-01-18 06:37:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Лицензия: MIT License
 # Автор: Азат Фазылов
 
-#Описание: Это программа для распознавания лиц на изображениях и сравнения их
-#с известными лицами. Она использует библиотеку face_recognition для
-#обнаружения и кодирования лиц на фотографиях.
+# Описание: Это программа для распознавания лиц на изображениях и сравнения их
+# с известными лицами. Она использует библиотеку face_recognition для
+# обнаружения и кодирования лиц на фотографиях.
 
 # Импорт необходимых библиотек
 
@@ -14,7 +14,6 @@ from PIL import Image, ImageDraw
 import numpy as np
 from shutil import copyfile
 import shutil
-import time
 
 # Функция для сравнения лиц
 def compare_faces(known_face_encoding, face_encoding_to_check, tolerance=0.55):
@@ -79,92 +78,6 @@ def recognize_faces_in_folder(folder_path, output_folder, known_faces):
     # Перебор всех файлов
     for image_file in image_files:
         image_path = os.path.join(folder_path, image_file)
-
-        # Загрузка изображения с лицами
-        image = face_recognition.load_image_file(image_path)
-
-        # Нахождение всех лиц на изображении
-        face_locations = face_recognition.face_locations(image)
-        face_encodings = face_recognition.face_encodings(image, face_locations)
-
-        # Определение имен и сохранение параметров по распознанным лицам
-        recognized_faces = []
-        for (top, right, bottom, left), face_encoding in zip(
-            face_locations, face_encodings
-        ):
-            face_data = {
-                "name": os.path.abspath(image_path),  # Используйте абсолютный путь
-                "encoding": face_encoding.tolist(),
-            }
-            recognized_faces.append(face_data)
-
-        # Отрисовка прямоугольников вокруг лиц
-        pil_image = Image.fromarray(image)
-        draw = ImageDraw.Draw(pil_image)
-
-        for (top, right, bottom, left), face_data in zip(
-            face_locations, recognized_faces
-        ):
-            draw.rectangle([left, top, right, bottom], outline="red", width=5)
-            draw.text((left, top - 10), face_data["name"], fill="red")
-
-        # Сохранение параметров каждого распознанного лица в JSON
-        save_path = os.path.join(
-            output_folder, f"recognized_faces_{image_file.replace('.', '_')}.json"
-        )
-        with open(save_path, "w") as json_file:
-            json_file.write(json.dumps(recognized_faces, indent=2))
-
-        # Сохранение результата изображения с выделенными лицами
-        pil_image.save(os.path.join(output_folder, f"recognized_faces_{image_file}"))
-
-        # Вызов функции для сравнения и сохранения похожих лиц
-        save_similar_faces(known_faces, output_folder, recognized_faces)
-
-
-# Пример использования
-# Путь к папке с известными лицами
-known_faces_folder = "known_faces"
-
-# Путь к папке с изображениями
-folder_path = "faces"
-
-# Путь к папке с результатом
-output_folder = "recognized"
-
-
-# Отчистка папки с резултьтатами
-if os.path.exists(output_folder):
-    shutil.rmtree(output_folder)
-
-    # Создание пустой папки recognized
-    os.makedirs(output_folder)
-
-# Загрузка известных лиц и их кодирований
-known_faces = []
-known_face_files = [
-    f for f in os.listdir(known_faces_folder) if f.lower().endswith(".json")
-]
-for known_face_file in known_face_files:
-    known_face_path = os.path.join(known_faces_folder, known_face_file)
-    with open(known_face_path, "r") as json_file:
-        known_face_data = json.load(json_file)
-
-        # Обработка каждого лица в файле
-        for face in known_face_data:
-            if "encoding" in face:
-                known_faces.append(
-                    {"encoding": np.array(face["encoding"]), "name": face["name"]}
-                )
-            else:
-                print(f"Invalid format in {known_face_file}. Skipping...")
-
-# Путь к папке с изображениями
-folder_path = "faces"
-output_folder = "recognized"
-
-# Распознавание лиц во всех фотографиях из папки
-recognize_faces_in_folder(folder_path, output_folder, known_faces)
 
         # Загрузка изображения с лицами
         image = face_recognition.load_image_file(image_path)
